92f55b76b82c8b31e13a16b5e833baf460cfcd60
unknown <cervantesdeveloper@gmail.com>
2024-05-11 19:24:03 -0600

    I have added the menu for mobile

--- js/main.js
+++ js/main.js
@@ -3,5 +3,5 @@ import burger from "./modules/burger.js";
 const d =  document;
 
 d.addEventListener("DOMContentLoaded", e=>{
-    burger(".burger")
+    burger(".burger", ".menu")
 })
--- js/modules/burger.js
+++ js/modules/burger.js
@@ -1,9 +1,10 @@
 const d =  document;
 
-export default function burger(burger){
+export default function burger(burger, menu){
     d.addEventListener("click", e=>{
         if(e.target.matches(burger) || e.target.matches(`${burger} *`)){
             d.querySelector(burger).classList.toggle("burger-open");
+            d.querySelector(menu).classList.toggle("menu-open")
         }
     })
 
