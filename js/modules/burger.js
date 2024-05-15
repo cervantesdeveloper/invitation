e52855359beade4d5948b8130863ee84d3fb409c
unknown <cervantesdeveloper@gmail.com>
2024-05-15 07:01:24 -0600

    I edited the burger functionality in order to close the menu when the click target is a link

--- js/modules/burger.js
+++ js/modules/burger.js
@@ -2,10 +2,10 @@ const d =  document;
 
 export default function burger(burger, menu){
     d.addEventListener("click", e=>{
-        if(e.target.matches(burger) || e.target.matches(`${burger} *`)){
+        if(e.target.matches(burger) || e.target.matches(`${burger} *`) || e.target.matches(`${menu} *`)){
             d.querySelector(burger).classList.toggle("burger-open");
             d.querySelector(menu).classList.toggle("menu-open")
         }
     })
-
+    console.log(`menu `)
 }
